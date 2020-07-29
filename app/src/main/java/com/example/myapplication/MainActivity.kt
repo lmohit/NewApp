3a154ada572d8b1ad4cf79bd221bff9875dd7747
Mohit Loganathan <lmohit95@gmail.com>
2020-07-29 22:15:27 +0530

    New Project Start

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,11 @@
 package com.example.myapplication
 
-import android.app.Activity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : FragmentActivity() {
 
-    private lateinit var viewModel: SampleViewModel
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding  = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProviders.of(this).get(SampleViewModel::class.java)
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = this
     }
 }
--- app/src/main/java/com/example/myapplication/MarsData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication
-
-data class MarsData(
-    val price: Double
-
-
-)
