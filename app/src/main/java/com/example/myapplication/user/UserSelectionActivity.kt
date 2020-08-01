6819ef3d6a4c22c696a913354bae937403741b63
Mohit Loganathan <lmohit95@gmail.com>
2020-08-01 21:24:40 +0530

    Adding UserSelection, Customer and Vendor Activity

--- app/src/main/java/com/example/myapplication/user/UserSelectionActivity.kt
+++ app/src/main/java/com/example/myapplication/user/UserSelectionActivity.kt
@@ -1,12 +1,34 @@
 package com.example.myapplication.user
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityUserSelectionBinding
 
-class UserSelectionActivity : AppCompatActivity() {
+class UserSelectionActivity : FragmentActivity() {
+
+    private lateinit var binding: ActivityUserSelectionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_selection)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_user_selection)
+        binding.lifecycleOwner = this
+
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        binding.customerButton.setOnClickListener {
+            val intent = Intent(applicationContext, CustomerActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.vendorButton.setOnClickListener {
+            val intent = Intent(applicationContext, VendorActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
